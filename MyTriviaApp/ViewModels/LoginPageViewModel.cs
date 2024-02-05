010dad854a678eda2ba66cb79cae380517473afd
User <User@LAB3-3>
2024-02-05 13:09:19 +0200

    New

---
+++ MyTriviaApp/ViewModels/LoginPageViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace MyTriviaApp.ViewModels
+{
+    internal class LoginPageViewModel:ViewModel
+    {
+       private string username;
+        public string UserName { get { return username; } set { username = value;OnPropertyChanged(); ((Command)CheckUsername).ChangeCanExecute(); } }
+
+        public ICommand CheckUsername {  get; set; }
+    }
+}
