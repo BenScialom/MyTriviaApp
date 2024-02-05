3b9277f49295697c646ac48d4ba0b873e4494330
User <User@LAB3-3>
2024-02-05 14:53:55 +0200

    New

--- MyTriviaApp/Service/Service.cs
+++ MyTriviaApp/Service/Service.cs
@@ -5,10 +5,49 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
 namespace MyTriviaApp.Service
 {
     public class Service
     {
        List<User> users = new List<User>();
+        public Service()
+        {
+           this.users=new List<User>();
+            FillList();
+        }
+
+        private void FillList()
+        {
+            users.Add(new User());
+            users.Add(new User { Password = "1234", UserName = "Ben" });
+        }
+       private bool CheckUsername(string username)
+        {
+            for(int i = 0;i< users.Count; i++)
+            {
+                if (users[i].UserName == username)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+        private bool CheckPassword(string password)
+        {
+            for(int i = 0;i< users.Count; i++)
+            {
+                if (users[i].Password == password)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+        public bool CheckLogin(User user)
+        {
+            return (CheckUsername(user.UserName)&&CheckPassword(user.Password));
+        }
+
     }
 }
--- MyTriviaApp/ViewModels/LoginPageViewModel.cs
+++ MyTriviaApp/ViewModels/LoginPageViewModel.cs
@@ -10,8 +10,11 @@ namespace MyTriviaApp.ViewModels
     internal class LoginPageViewModel:ViewModel
     {
        private string username;
+        private string password;
         public string UserName { get { return username; } set { username = value;OnPropertyChanged(); ((Command)CheckUsername).ChangeCanExecute(); } }
-
+        public string Password { get { return password; } set { password = value;OnPropertyChanged();((Command)CheckPassword).ChangeCanExecute(); } }
+        public ICommand CheckPassword {  get; set; }
         public ICommand CheckUsername {  get; set; }
+        
     }
 }
